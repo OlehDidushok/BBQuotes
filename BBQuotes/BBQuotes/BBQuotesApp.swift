e09ac2cdee48e876b646d09c5304bbfa59188c74
Oleh Didushok <didushokov@gmail.com>
2024-12-17 16:23:27 +0200

    Initial setup, added images, added tabView

---
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Oleh on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes/BBQuotes/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  BBQuotes
+//
+//  Created by Oleh on 17.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            Text("Breaking Bad View")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Breaking Bad", systemImage: "tortoise")
+                }
+            
+            Text("Better Call Saul  View")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Better Call Saul", systemImage: "briefcase")
+                }
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
